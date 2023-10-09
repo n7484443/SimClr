1bb860bcbab42b93ee298a656daedbb41be683ff
n7484443 <n7484443@naver.com>
2023-10-10 00:05:12 +0900

    수정

--- main_with_originalres.py
+++ main_with_originalres.py
@@ -49,98 +49,18 @@ def distortion(tensor_image: Tensor, strength: int) -> Tensor:
     return transform_several.forward(tensor_image)
 
 
-# identity resnet 구현
-class ResnetBlock(nn.Module):
-    def __init__(self, in_channel, out_channel):
-        super().__init__()
-        if in_channel != out_channel:
-            self.sequentialLayer = nn.Sequential(
-                nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=2, bias=False, padding=1),
-                nn.BatchNorm2d(out_channel),
-                nn.ReLU(),
-                nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, bias=False, padding=1),
-                nn.BatchNorm2d(out_channel)
-            )
-            # 1x1 conv, stride = 2(pooling을 안 쓰는 건 일종의 철학), 크기 조절을 위한 projection mapping
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=2, bias=False),
-                nn.BatchNorm2d(out_channel)
-            )
-        else:
-            self.sequentialLayer = nn.Sequential(
-                nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=1, bias=False, padding='same'),
-                nn.BatchNorm2d(out_channel),
-                nn.ReLU(),
-                nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, bias=False, padding='same'),
-                nn.BatchNorm2d(out_channel)
-            )
-            self.shortcut = nn.Sequential()
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-        x = self.sequentialLayer(x) + self.shortcut(x)
-        x = self.relu(x)
-        return x
-
-
-# conv output size = ceil((i + 2p - k) / s) + 1
-class Resnet(nn.Module):
-    def __init__(self, size):
-        super().__init__()
-        # rgb 3개로 16개의 채널 만듬
-        # 16 * image width(32) * image height(32)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, bias=False, padding=1),
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-        )
-        # 16 * 32 * 32 -> 32 * 16 * 16
-        self.conv2 = nn.Sequential(
-            ResnetBlock(16, 32),
-            *[ResnetBlock(32, 32) for _ in range(4)]
-        )
-        # 32 * 16 * 16 -> 64 * 8 * 8
-        self.conv3 = nn.Sequential(
-            ResnetBlock(32, 64),
-            *[ResnetBlock(64, 64) for _ in range(4)]
-        )
-        # 64 * 8 * 8 -> 128 * 4 * 4
-        self.conv4 = nn.Sequential(
-            ResnetBlock(64, 128),
-            *[ResnetBlock(128, 128) for _ in range(4)]
-        )
-        # 전체 평균, 즉 128 * 1 * 1
-        # AvgPool2d 는 Conv2d와 같이 커널을 이동시키며 평균을 구함
-        # AdaptiveAvgPool2d 는 AvgPool2d와 비슷하나 특정 크기로 자동으로 맞춰줌
-        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        # Flatten으로 128 * 1 * 1을 128 벡터로 바꾼 후 fully connected layer로 128을 size 크기로 변환
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128, size)
-        )
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.conv3(x)
-        x = self.conv4(x)
-        x = self.avg_pool(x)
-        x = self.fc(x)
-        return x
-
-
 class SimpleMLP(nn.Module):
     # SimCLR에 사용된 최소화된 MLP
     def __init__(self, input_size, output_size):
         super().__init__()
         self.input = nn.Linear(input_size, output_size)
-        self.relu = nn.ReLU()
         self.hidden = nn.Linear(output_size, output_size)
+        self.relu = nn.ReLU()
 
     def forward(self, x):
         x = self.input(x)
-        x = self.relu(x)
         x = self.hidden(x)
+        x = self.relu(x)
         return x
 
 
@@ -234,7 +154,7 @@ if __name__ == '__main__':
     writer = SummaryWriter()
     want_train = True
     device = torch.device("cuda")
-    hyper_batch_size = 50
+    hyper_batch_size = 500
     hyper_epoch = 50
 
     testLoader: tu_data.DataLoader
@@ -244,15 +164,18 @@ if __name__ == '__main__':
     # rgb 3개,
     size = 32
     output_size = 10
-    f_resnet = Resnet(size=size)
+    f_resnet = torchvision.models.resnet18(pretrained=False)
+    num_input = f_resnet.fc.in_features
+    f_resnet.fc = nn.Linear(num_input, size)
+    f_resnet.to(device)
+
     loss_function = SimCLR_Loss(batch_size=hyper_batch_size, temperature=0.1)
 
     summary(f_resnet, input_size=(hyper_batch_size, 3, 32, 32))
 
-    f_resnet.to(device)
-    if os.path.exists('./model.pt'):
-        f_resnet = torch.load('./model.pt')
-    if not os.path.exists('./model.pt') or want_train:
+    # if os.path.exists('./model.pt'):
+        # f_resnet = torch.load('./model.pt')
+    if not os.path.exists('./model.pt') or False:
         g_small = SimpleMLP(input_size=size, output_size=output_size)
         g_small.to(device)
         fg = nn.Sequential(
@@ -260,7 +183,7 @@ if __name__ == '__main__':
             g_small
         )
         fg.train()
-        optimizer = torch.optim.Adam(fg.parameters(), lr=0.0001)
+        optimizer = torch.optim.Adam(fg.parameters(), lr=0.001)
         scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95 ** epoch)
         # train
         for epoch in range(hyper_epoch):
@@ -319,15 +242,14 @@ if __name__ == '__main__':
         simple_loss_function = nn.CrossEntropyLoss()
         simple_loss_function.to(device)
         predictor.train()
-        optimizer = torch.optim.Adam(predictor.parameters(), lr=0.001)
+        optimizer = torch.optim.Adam(predictor.parameters(), lr=0.1)
         scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer=optimizer, lr_lambda=lambda epoch: 0.95 ** epoch)
         for epoch in range(hyper_epoch):
             # batch_size * 2, rgb, x, y 의 데이터 형태
             loss_sum = 0
             for batch_data, batch_label in trainLoader:
                 batch_size = batch_data.shape[0]
-                with torch.no_grad():
-                    output = f_resnet.forward(batch_data.to(device))
+                output = f_resnet.forward(batch_data.to(device))
                 expect = predictor.forward(output)
                 loss = simple_loss_function(expect, batch_label.to(device))
                 loss_sum += loss.item()
